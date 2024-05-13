3398ae68a5818f55b0e7357c658f4a4684a8b30a
David Yang <yang.zhiyu@northeastern.edu>
2024-05-13 09:06:32 -0700

    update upscale code

--- image_upscale.py
+++ image_upscale.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 stability_api = client.StabilityInference(
-    key=os.getenv("STABILITY_API_KEY"), # Your Stability API key.
+    key=os.environ.get("STABILITY_API_KEY"), # Your Stability API key.
     upscale_engine="esrgan-v1-x2plus", # The name of the upscaling model we want to use.
                                        # Available Upscaling Engines: esrgan-v1-x2plus
     verbose=True, # Print debug messages.
@@ -20,7 +20,7 @@ stability_api = client.StabilityInference(
 def resize_to_target_size(image_path, target_size):
     print("Resizing image to target size ...")
     image = Image.open(image_path)
-    image = image.resize(target_size, Image.ANTIALIAS)
+    image = image.resize(target_size, Image.Resampling.LANCZOS)
     return image
 
 def upscale_image(image_path, output_path):
@@ -45,6 +45,9 @@ if __name__ == "__main__":
     parser.add_argument("--image_path", type=str, help="Path to the image to upscale.")
     parser.add_argument("--target_size", type=int, nargs='+', help="Target size for the image.")
     args = parser.parse_args()
-    upscale_image(args.image_path, "upscaled_image.png")
-    resize_to_target_size("upscaled_image.png", tuple(args.target_size))
+    # check if dir samples exists
+    if not os.path.exists("samples"):
+        os.makedirs("samples")
+    upscale_image(args.image_path, "samples/upscaled_image.png")
+    resize_to_target_size("samples/upscaled_image.png", tuple(args.target_size))
     print("Image upscaled and resized successfully!")
